868b0500f83471226bbe986328330edad8f8ab3c
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-09-05 16:15:01 +0530

    Time: 90 ms (8.01%), Space: 14.3 MB (55.53%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -34,12 +34,11 @@ class Solution:
                     
             else:
                 
-                if target<nums[mid] or target>nums[r]:
-                    
-                    r=mid-1
-                
-                else:
+                if target>nums[mid] and target<=nums[r]:
                     l=mid+1
+                    
+                else:
+                    r=mid-1
                     
         return -1           
                 

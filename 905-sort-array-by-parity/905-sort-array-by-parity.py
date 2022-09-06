64757336d320315d0d228286831347804b88c50c
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-09-06 19:35:12 +0530

    Time: 200 ms (5.08%), Space: 14.7 MB (58.21%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -24,14 +24,14 @@ class Solution:
         l,r=0,len(nums)-1
         
         
-        while l<r:
+        while l<=r:
             
             
-            while nums[l]%2==0 and l<r:
+            while nums[l]%2==0 and l<r:          ## traaverse till u get an odd number   
                 
                 l+=1
                 
-            while nums[r]%2!=0 and l<r:
+            while nums[r]%2!=0 and l<r :       ## traverse till u get an even number
                 
                 r-=1
             

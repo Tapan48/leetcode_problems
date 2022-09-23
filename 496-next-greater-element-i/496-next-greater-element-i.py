5d6103e0842512718a262c452c0b1640df480706
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-09-23 09:56:44 +0530

    Time: 117 ms (29.60%), Space: 14.2 MB (56.88%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,7 +1,7 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
-        
+        """
         for i in range(len(nums1)):
             for j in range(len(nums2)):
                 
@@ -15,6 +15,27 @@ class Solution:
                     if k==len(nums2):
                         nums1[i]=-1
                     break    
-        return nums1                
+        return nums1
+    """
+        
+        res=[-1]*len(nums1)
+        
+        dictt={num:i for i,num in enumerate(nums1)}
+        
+        for i in range(len(nums2)):
+            
+            if nums2[i] in dictt:
+                
+                for j in range(i+1,len(nums2)):
+                    
+                    if nums2[j]>nums2[i]:
+                        
+                        ind=dictt[nums2[i]]
+                        res[ind]=nums2[j]
+                        break
+        return res                          
+                    
+       
+    
                             
         

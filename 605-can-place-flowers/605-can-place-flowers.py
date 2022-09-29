89481c620f384c7d811d0bc8dbb088e5c65e946b
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-09-29 23:10:05 +0530

    Time: 395 ms (19.74%), Space: 14.2 MB (93.19%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.py
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -1,7 +1,7 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         
-        
+        """
         for i in range(0,len(flowerbed)):
             
             if len(flowerbed)==1:
@@ -24,5 +24,23 @@ class Solution:
                     n-=1
                     
         return n<=0            
-          
+          """
+        
+        
+        arr=[0]+flowerbed+[0]
+        
+        for i in range(1,len(arr)-1):
+            
+            
+            if arr[i-1]==0 and arr[i]==0 and arr[i+1]==0:
+                arr[i]=1
+                n-=1
+                
+        return n<=0        
+        
+        
+        
+        
+        
+        
         

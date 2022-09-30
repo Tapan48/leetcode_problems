52a20884fa455f335c39ce32381f814a03f752ee
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-01 00:27:03 +0530

    Time: 8592 ms (27.57%), Space: 13.9 MB (92.87%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,27 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows=len(board)
+        cols=len(board[0])
+        path=set()
+        
+        
+        def dfs(row,col,i):
+            if i==(len(word)):
+                return True
+            
+            if (row<0 or col<0 or row>=rows or col>=cols or word[i]!=board[row][col]or (row,col)in path):
+                return False
+            
+            path.add((row,col))
+            
+            res=dfs(row,col+1,i+1)or dfs(row,col-1,i+1)or dfs(row+1,col,i+1)or dfs(row-1,col,i+1)
+            path.remove((row,col))
+            return res
+        
+        
+        for row in range(rows):
+            for col in range(cols):
+                if dfs(row,col,0):
+                    return True
+                
+        return False        

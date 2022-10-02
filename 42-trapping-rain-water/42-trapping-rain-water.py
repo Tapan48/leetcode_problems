19bf3e5812022091e65c4f5524d29b1f82a67149
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-02 21:06:51 +0530

    Time: 270 ms (40.22%), Space: 16 MB (81.80%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,6 +1,6 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        
+        """
         n=len(height)
         maxleft=[0 for i in range(n)]
         
@@ -36,6 +36,42 @@ class Solution:
                 
                 
         return sum(height)        
+            """
+        n=len(height)
+        l=0
+        r=n-1
+        maxl=height[0]
+        maxr=height[r]
+        
+        while l<=r:
+            if maxl <=maxr:
+                
+                val=maxl-height[l]
+                maxl=max(maxl,height[l])
+                if val<=0:
+                    height[l]=0
+                    
+                else:
+                    height[l]=val
+                l+=1
+                
+            else:
+                val=maxr-height[r]
+                maxr=max(maxr,height[r])
+                if val<=0:
+                    height[r]=0
+                    
+                else:
+                    height[r]=val
+                r-=1
+        print(height)        
+        return sum(height)       
+                
+                
             
-       
+            
+            
+        
+            
+            
               

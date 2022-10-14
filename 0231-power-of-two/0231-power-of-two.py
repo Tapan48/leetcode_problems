d1d845c05160b5b2b7e21a479d576890711bd4c9
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-15 00:02:53 +0530

    Time: 68 ms (21.89%), Space: 13.9 MB (9.58%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         
-        
+        """
         def recfunc(n):
             if n==1:  
                 return True
@@ -14,22 +14,24 @@ class Solution:
                 return False
         res=recfunc(n)
         return res
-        
-        
-        
         """
+        
+        
+        
     ## iterative soltn
         while n>=0:
             
             if n==1:
                 return True
+            elif n%2!=0:
+                return False
             
             elif n==0:
                 return False
             
             n/=2
          
-         """
+         
         
         """
         return n>0 and n & (n-1)==0          ##     if n is power of two then

6608bcd5b84b6921e0649241c7b34a0b7da982ef
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-15 16:06:28 +0530

    Time: 46 ms (71.62%), Space: 13.9 MB (7.87%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -4,9 +4,15 @@ class Solution:
         cnt=0
         
         
-        while n:
+        while n:                    ## used right shift operator
+                                     ## 1101>>1  (means right shift every bit by 1 step )
+                                     ## 0110>>1
+                                     ## 0011>>1
+                                     ## 0001>>1 
+                                     ## 0000     
             
-            cnt+=n%2
+            cnt+=n%2                ## if last digit is 1 then n%2 =1 
+                                    ## if last digit is 0 then n%2=0
             
             n=n>>1
             

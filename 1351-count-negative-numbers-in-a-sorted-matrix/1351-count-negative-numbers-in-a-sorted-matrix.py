da78e9a372e0d7818e2e2a49c3f8e9b3af052eba
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-16 19:57:31 +0530

    Time: 363 ms (5.04%), Space: 15.2 MB (10.58%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -10,14 +10,21 @@ class Solution:
                     
         return cnt
     """
-        
-        nr,nc=len(grid),len(grid[0])
+        """
+        nr,nc=len(grid),len(grid[0])         ###  
         i,j=0,nc-1
         cnt=0
         
-        while i<len(grid) and j>=0:
+        while i<len(grid) and j>=0:        
             
-            if grid[i][j]>=0:
+            
+            if grid[i][j]>=0:                ## we start iterating from top rightest element 
+                                            ## if element is +ve then all elements present 
+                                            ## at left side of element is +ve,so we increment                                             ## the row
+                                            ##
+                            
+                                            ##if element is -ve all elements below it in that                                             ## column will be -ve as col sorted in desc order
+                                             ## cnt all -ve values and decrement col
                 i+=1
                 
             else:
@@ -26,7 +33,21 @@ class Solution:
                 
                 
         return cnt        
-            
+           """
+        def bin(row):
+            start, end = 0, len(row)
+            while start<end:
+                mid = start +(end -start) // 2
+                if row[mid]<0:
+                    end = mid
+                else:
+                    start = mid+1
+            return len(row)- start
+        
+        count = 0
+        for row in grid:
+            count += bin(row)
+        return(count)
         
     
     

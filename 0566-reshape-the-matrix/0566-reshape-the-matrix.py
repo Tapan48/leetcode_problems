20a2c3be56d90e48b8c73745ace4b133998518c3
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-19 00:01:00 +0530

    Time: 207 ms (29.57%), Space: 15.2 MB (9.05%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -0,0 +1,32 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        
+        
+        
+        nr=len(mat)
+        
+        nc=len(mat[0])
+        
+        arr=[[0 for i in range(c)]for j in range(r)]
+        
+        if (nr*nc)==(r*c):
+            x,y=0,0
+            for i in range(r):
+                for j in range(c):
+                    arr[i][j]=mat[x][y]
+                    
+                    if y<nc:
+                        y+=1
+                        
+                    if y==nc:
+                        y=0
+                        
+                        if x<nr:
+                            x+=1
+            return arr                   
+        
+        else:
+            return mat
+            
+            
+            

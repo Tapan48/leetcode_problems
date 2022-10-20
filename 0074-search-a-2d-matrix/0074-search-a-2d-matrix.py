295594b7c3cbaa7e01935780929025cc49d40781
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-20 17:06:48 +0530

    Time: 72 ms (8.27%), Space: 13.9 MB (47.41%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for row in matrix:
+            
+            if target<=row[len(row)-1]:
+                
+                l,r=0,len(row)-1
+                
+                while l<=r:
+                    
+                    mid=(l+r)//2
+                    
+                    if row[mid]==target:
+                        return True
+                    
+                    elif target>row[mid]:
+                        l=mid+1
+                    
+                    else:
+                        r=mid-1
+                        
+                        
+                        

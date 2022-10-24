a21527979b9cfef182094a3e86d6663cfc1278d2
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-25 00:17:50 +0530

    Time: 520 ms (10.56%), Space: 15.4 MB (21.12%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -1,8 +1,7 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         
-        if len(nums)==1:
-            return True
+      
         
         for i in range(len(nums)-1):
             

929f9335bcfda4ef02ee47c60279c1d3db31448b
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-28 21:01:52 +0530

    Time: 2755 ms (23.93%), Space: 25 MB (85.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -19,17 +19,17 @@ class Solution:
             return maxprofit
                 """
         
-        
+        """
         l,r=0,1
         maxprofit=0
         
-        while r<len(prices):
+        while r<len(prices):                          ## tc o(n)  sc o(1)
             
             
-            if prices[l]>prices[r]:
-                l=r
-                r+=1
-                
+            if prices[l]>prices[r]:                      ## profit=sp -cp
+                l=r                                      ##  for maxprofit , sp high
+                r+=1                                            ## cp low
+                 
             else:
                 
                 profit=prices[r]-prices[l]
@@ -38,7 +38,39 @@ class Solution:
                 r+=1
                 
         return maxprofit       
+           """
+        
+        
+        
+        
+        
+        res=[0]*len(prices)
+        
+        maxright=0
+        for i in range(len(prices)-1,-1,-1):
+            
+            if prices[i]>maxright:
+                maxright=max(maxright,prices[i])
                 
+                res[i]=prices[i]
+                
+            else:
+                res[i]=maxright
+                
+        maxprofit=0        
+        for i in range(len(prices)):
+            
+            
+            profit=res[i]-prices[i]
+            
+            maxprofit=max(profit,maxprofit)
+                
+        return maxprofit        
+                
+                
+                
+            
+        
                 
         
         

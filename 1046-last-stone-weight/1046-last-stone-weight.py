2a7144a0743a79e0ad219317a91598406057fc8b
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-29 00:12:10 +0530

    Time: 64 ms (30.38%), Space: 13.8 MB (96.86%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,8 +1,9 @@
+
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         
         
-        
+        """
         stones.sort()
         
         
@@ -18,10 +19,36 @@ class Solution:
                 stones.sort()
                 
         if len(stones)==0:
-            return 0
+            return 0 
         else:
             return stones[0]
+        """
         
+        
+        stonesnew=[-x for x in stones]
+        heapq.heapify(stonesnew)
+        
+        while len(stonesnew)!=1 and len(stonesnew)>0:
+            
+           
+            print(stonesnew)
+            larg1=abs(heapq.heappop(stonesnew))
+            larg2=abs(heapq.heappop(stonesnew))
+            
+            
+            if larg1!=larg2:
+                
+                diff=larg1-larg2
+                val=(-1)*diff
+                heapq.heappush(stonesnew,val)
+                
+        if len(stonesnew)==0:
+            return 0
+        else:
+            return abs(stonesnew[0])       
+                
+                
+            
         
         
         

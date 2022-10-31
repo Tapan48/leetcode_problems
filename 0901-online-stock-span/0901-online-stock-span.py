15d36e2021b3ad32d59ea5aae27c12a1badbfbd2
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-10-31 15:27:13 +0530

    Time: 1082 ms (26.55%), Space: 19.6 MB (14.86%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.py
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -7,32 +7,21 @@ class StockSpanner:
 
     def next(self, price: int) -> int:
         
-        if not(self.stack):
-            self.stack.append((price,1))
-            return 1
-        
-        
-        
-        else:
+                     
+          
             
             cnt=1
-            
-            if self.stack[-1][0]>price:
                 
-                self.stack.append((price,1))
-                
-                
-                return 1
-            
-            else:
-                
-                while self.stack and self.stack[-1][0]<=price:
+            while self.stack and self.stack[-1][0]<=price:
                     
                     prevprice,prevspan=self.stack.pop()
                     cnt+=prevspan
                     
-                self.stack.append((price,cnt))  
-                return cnt
+            self.stack.append((price,cnt))  
+            return cnt
+        
+           
+          
                 
                     
                     

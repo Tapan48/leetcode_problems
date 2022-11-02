10ed7fe465b4ecb9ed0a25d881c016b00843f33a
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-11-03 00:57:27 +0530

    Time: 237 ms (44.23%), Space: 19.5 MB (28.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
+        """
         dictt={}
         for i,num in enumerate(nums):       ## tc o(nlog n) sc o(n) 
                                             ## created a dictionary and stored numbers 
@@ -19,6 +20,43 @@ class Solution:
         res=list(resdict.keys())[0:k]
         
         return res
+        """
         
+        
+        freq=Counter(nums)
+        
+        res=[[] for i in range(len(nums)+1)]
+        
+        
+        for element,count in freq.items():    ## indices are counts and their values are                                                                    elements
+            
+            res[count].append(element)
+            
+        arr=[]
+        
+        for i in range(len(res)-1,-1,-1):
+            
+            
+            for n in res[i]:
+                arr.append(n)
+                
+            if len(arr)==k:
+                return arr
+            
+            
+            
+        
+        
+        return arr
+        
+            
+            
+            
+            
+            
+            
+        
+        
+            
         
         

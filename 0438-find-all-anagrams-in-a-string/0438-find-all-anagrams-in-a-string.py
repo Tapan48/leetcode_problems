70cf60f78e4e4c38453e9491db5d00df30f7df1b
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-11-02 14:50:19 +0530

    Time: 119 ms (93.94%), Space: 15.3 MB (5.69%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,8 +1,8 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         
-        
-        if len(p)>len(s):
+        """
+        if len(p)>len(s):                   ## my code 
             return []
         dicp={}
         for i in range(len(p)):
@@ -13,6 +13,7 @@ class Solution:
         
         dics={}                    ## problem statement is to check if every string in s 
         for i in range(len(p)):              ## is an anagram of p 
+                                       
             
             dics[s[i]]=dics.get(s[i],0)+1
             
@@ -38,7 +39,51 @@ class Solution:
             
         return res     
             
+           """
+        
+        
+        if len(p)>len(s):
+                return []                          ## neetcode referred code
+        
+        
+        
+        dicp,dics={},{}
+        
+        for i in range(len(p)):
+            
+            dicp[p[i]]=dicp.get(p[i],0)+1
+            dics[s[i]]=dics.get(s[i],0)+1
+            
+        res=[0] if dicp==dics else []
+        
+        l=0
+        for r in range(len(p),len(s)):
+            
+            dics[s[r]]=dics.get(s[r],0)+1      ## put new character at r index in dics and
+                                              ##  increment l pointer
+            
+            
+            
+            dics[s[l]]-=1
+            
+            
+            if dics[s[l]]==0:
                 
+                dics.pop(s[l])
+            
+            l+=1
+            
+            if dics==dicp:
+                
+                res.append(l)
+                
+        return res        
+                
+            
+        
+            
+            
+            
                 
                 
             

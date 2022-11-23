8be4e8a9548e531ebaada2d07f295207ed096b0a
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2022-11-23 13:44:07 +0530

    Time: 941 ms (7.34%), Space: 16.7 MB (34.65%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,85 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        
+        
+        
+        
+        prefixsum={0:1}
+        
+        cursum,res=0,0
+        
+        for num in nums:
+            
+            
+            cursum+=num
+            
+            diff=cursum-k
+            
+            
+            if diff in prefixsum.keys():
+                
+                res+=prefixsum[diff]
+                
+            prefixsum[cursum]=prefixsum.get(cursum,0)+1    
+                
+                
+        return res         
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         cnt=0                            ## hashmap is used
+#         for i in range(len(nums)):
+            
+#             summ=0
+#             for j in range(i,len(nums)):
+                
+               
+                        
+                
+#                     summ+=nums[j]
+#                     if summ>k:
+#                         break
+                    
+#                     if summ==k:
+#                         cnt+=1
+                    
+                    
+                        
+                    
+                    
+#         return cnt           
+                    
+                    
+                        
+                        
+                
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        

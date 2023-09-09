7af709af7aa59f35acd380e26a8f7018ebdc134a
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-09-09 22:56:35 +0530

    Time: 58 ms (7.07%), Space: 17.4 MB (7.74%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -3,50 +3,79 @@ class Solution:
 
 ###  openmax and openmin variables used to solve this question
 
+            dp={}
+            def dfs(i,netopen):
+        
+                if i==len(s):
+                   return netopen==0
+                
+                if (i,netopen)in dp:
+                    return dp[(i,netopen)]
+                
+                if netopen<0:
+                    return False
+        
+                    
+                if s[i]=="(":
+                    
+                    
+                     dp[(i,netopen)]= dfs(i+1,netopen+1)
+                    
+                elif s[i]==")":
+                        dp[(i,netopen)]= dfs(i+1,netopen-1)
+                elif s[i]=="*":
+                    dp[(i,netopen)]= dfs(i+1,netopen+1)or dfs(i+1,netopen-1)or dfs(i+1,netopen)
+                    
+                return dp[(i,netopen)]    
+            return   dfs(0,0)  
+                 
+                
+                
+                
+                
+                    
+                    
+
 
         
-            dp = {}
+#             dp = {}
 
-            def dfs(i, num_open):
-                if (i, num_open) in dp:
-                    return dp[(i, num_open)]
+#             def dfs(i, num_open):
+#                 if (i, num_open) in dp:
+#                     return dp[(i, num_open)]
 
-                if num_open < 0:
-                    dp[(i, num_open)] = False
-                elif i == len(s):
-                    dp[(i, num_open)] = num_open == 0
-                elif s[i] == '(':
-                    dp[(i, num_open)] = dfs(i + 1, num_open + 1)
-                elif s[i] == ')':
-                    dp[(i, num_open)] = dfs(i + 1, num_open - 1)
-                elif s[i] == '*':
-                    dp[(i, num_open)] = (
-                        dfs(i + 1, num_open + 1) or
-                        dfs(i + 1, num_open - 1) or
-                        dfs(i + 1, num_open)
-                    )
+#                 if num_open < 0:
+#                     dp[(i, num_open)] = False
+#                 elif i == len(s):
+#                     dp[(i, num_open)] = num_open == 0
+#                 elif s[i] == '(':
+#                     dp[(i, num_open)] = dfs(i + 1, num_open + 1)
+#                 elif s[i] == ')':
+#                     dp[(i, num_open)] = dfs(i + 1, num_open - 1)
+#                 elif s[i] == '*':
+#                     dp[(i, num_open)] = (
+#                         dfs(i + 1, num_open + 1) or
+#                         dfs(i + 1, num_open - 1) or
+#                         dfs(i + 1, num_open)
+#                     )
 
-                return dp[(i, num_open)]
+#                 return dp[(i, num_open)]
 
-            return dfs(0, 0)
+#             return dfs(0, 0)
+
+               
+                    
+                    
+                   
+                   
+                    
+               
+               
 
             
 
         
-#         def dfs(i, num_open):   ### not working
-            
-#             if i == len(s) and num_open == 0:
-#                 return True
-#             if i == len(s) and num_open < 0:
-#                 return False
-#             elif  s[i] == "*":
-#                 return dfs(i + 1, num_open + 1) or dfs(i + 1, num_open) or dfs(i + 1, num_open - 1)
-#             elif  s[i] == "(":
-#                 return dfs(i + 1, num_open + 1)
-#             elif  s[i] == ")":
-#                 return dfs(i + 1, num_open - 1)
-
-#         return dfs(0, 0)
+    
 
 
 

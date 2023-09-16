6b0b24d10273e8c6ff8b0d83ca369da6f490f071
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-09-16 15:04:49 +0530

    Time: 917 ms (14.18%), Space: 41.9 MB (12.65%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -2,23 +2,32 @@ class MyHashMap:
 
     def __init__(self):
         
-        self.dict={}
+        # self.dict={}
+        self.array=[-1 for i in range(1000001)]
+        
 
     def put(self, key: int, value: int) -> None:
         
-        self.dict[key]=value
+        # self.dict[key]=value
+        
+        self.array[key]=value
         
 
     def get(self, key: int) -> int:
         
-        return self.dict[key] if key in self.dict else -1
+        # return self.dict[key] if key in self.dict else -1
+        
+        return self.array[key]
         
         
         
 
     def remove(self, key: int) -> None:
         
-        self.dict.pop(key) if key in self.dict else self.dict
+        # self.dict.pop(key) if key in self.dict else self.dict
+        self.array[key]=-1
+        
+        
         
 
 

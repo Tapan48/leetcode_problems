32da0ace49c308c07d39bb2ed370e221160a041f
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-09-18 15:35:44 +0530

    Time: 86 ms (57.60%), Space: 24.2 MB (18.68%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,132 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        
+        
+        
+        
+        freq=Counter(nums)
+        
+        x=set(nums)
+        arr=list(x)
+        arr.sort()
+        
+        dp={}
+        
+        
+        def dfs(i):
+            
+            
+            if i==len(arr):
+                return 0
+            
+            if i in dp:
+                return dp[i]
+            
+            
+            
+            noreward=0
+            reward=arr[i]*freq[arr[i]]
+            
+            if i+1<len(arr):
+            
+               reward+=(dfs(i+2) if arr[i+1]==arr[i]+1 else dfs(i+1))
+                
+            else:
+                reward+=dfs(i+1)
+            
+            noreward+=(dfs(i+1))
+            
+            dp[i]=max(reward,noreward)
+            
+            return dp[i]
+        return dfs(0)  
+        
+        
+        
+        
+        
+        
+        
+
+
+#         freq=Counter(nums)
+
+
+
+#         nums.sort()
+
+
+#         x=set(nums)
+#         # arr = [0 for i in range(3)] 
+#         arr=list(x)
+
+
+#         dp=[1]*len(arr)
+
+#         for i,num in enumerate(arr):
+
+
+#             if i==0:
+
+#                 dp[i]=freq[num]
+
+#             else:
+
+
+#                 if arr[i]==(arr[i-1]+1):
+
+#                     arr[i]=max(arr[i-1],(arr[i]*freq[arr[i]])+arr[i-2] if  0<=i-2<len(arr))
+
+#                 else:
+#                     arr[i]=max(arr[i-1],(arr[i]*freq[arr[i]])+arr[i-1])
+
+#         return arr[-1]                
+
+                
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

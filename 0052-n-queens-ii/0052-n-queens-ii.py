ab3ee114cc81ff2d194474887f441699380b0337
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-09-19 22:05:53 +0530

    Time: 38 ms (97.86%), Space: 16.2 MB (96.40%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,9 +1,7 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
         
-        board=[["."]*n for i in range(n)]
-        
-        res=[]
+       
         
         posdiag=set()
         negdiag=set()
@@ -34,14 +32,14 @@ class Solution:
                 if  c in col or (r+c)in posdiag or (r-c)in negdiag:
                     continue
                     
-                board[r][c]="Q"
+     
                 posdiag.add(r+c)
                 negdiag.add(r-c)
                 col.add(c)
                 
                 backtracking(r+1)
                 
-                board[r][c]="."
+           
                 posdiag.remove(r+c)
                 negdiag.remove(r-c)
                 col.remove(c)

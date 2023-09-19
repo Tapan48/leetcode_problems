0090ed2826605f2b6fdd32ee052cf5fe1ea45395
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-09-19 21:47:42 +0530

    Time: 38 ms (97.86%), Space: 16.4 MB (50.57%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,52 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        
+        board=[["."]*n for i in range(n)]
+        
+        res=[]
+        
+        posdiag=set()
+        negdiag=set()
+        col=set()
+        
+        
+        
+        
+        
+        
+        
+        def backtracking(r):
+            
+            if r==n:
+                
+                copy= ["".join(row) for row in board]
+                
+                res.append(copy)
+                return 
+                
+                
+            
+            
+            
+            
+            
+            for c in range(n):
+                
+                if  c in col or (r+c)in posdiag or (r-c)in negdiag:
+                    continue
+                    
+                board[r][c]="Q"
+                posdiag.add(r+c)
+                negdiag.add(r-c)
+                col.add(c)
+                
+                backtracking(r+1)
+                
+                board[r][c]="."
+                posdiag.remove(r+c)
+                negdiag.remove(r-c)
+                col.remove(c)
+                
+        backtracking(0)
+        return len(res)
+        

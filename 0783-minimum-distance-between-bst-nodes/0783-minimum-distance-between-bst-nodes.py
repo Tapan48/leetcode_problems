a8bff23457b817636d94e5f9bd5023741f5d9e39
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-10-06 22:51:49 +0530

    Time: 49 ms (5.11%), Space: 16.2 MB (82.52%) - LeetHub

---
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        
+        arr=[]
+        def inorder(root):
+            
+            if not(root):
+                return 
+            
+            
+            
+            inorder(root.left)
+            arr.append(root.val)
+            inorder(root.right)
+        
+        inorder(root)
+        res=arr[len(arr)-1]
+        for i in range(len(arr)):
+            for j in range(i+1,len(arr)):
+                
+                diff=(arr[j]-arr[i])
+                
+                res=min(res,diff)
+        return res        
+                
+        

71f16c5889f1c73eb74b2aad60fb7f816de0ce99
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-10-07 00:20:36 +0530

    Time: 34 ms (87.08%), Space: 16.3 MB (82.52%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -7,25 +7,66 @@
 class Solution:
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
         
-        arr=[]
-        def inorder(root):
-            
+        
+        res=float("inf")
+        prev=None
+        def inorder(root):   ## recursive soltn
+            nonlocal prev,res
             if not(root):
                 return 
             
             
-            
             inorder(root.left)
-            arr.append(root.val)
-            inorder(root.right)
-        
-        inorder(root)
-        res=arr[len(arr)-1]
-        for i in range(len(arr)-1):
-              
-            diff=arr[i+1]-arr[i]
-            res=min(res,diff)
             
-        return res        
+            if prev:
+                res=min(res,root.val-prev.val)
+                
+            prev=root   
+            inorder(root.right)
+        inorder(root)
+        return res
+            
+            
+            
+            
+            
+            
+            
+            
+          
+        inorder(root)
+        return res
+            
+            
+            
+            
+          
+              
+               
+                
+                
+        inorder(root)
+        return x
+        
+#         arr=[]
+#         def inorder(root):    ### tc o(n) sc o(n)         
+            
+#             if not(root):
+#                 return 
+            
+            
+            
+#             inorder(root.left)
+#             arr.append(root.val)
+#             inorder(root.right)
+        
+#         inorder(root)
+#         res=arr[len(arr)-1]
+#         for i in range(len(arr)-1):
+              
+#             diff=arr[i+1]-arr[i]
+#             res=min(res,diff)
+            
+#         return res        
                 
         

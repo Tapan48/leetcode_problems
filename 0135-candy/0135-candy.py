ff32ee3644ea405c88f552b560ed33a916252d95
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-11-12 17:19:33 +0530

    Time: 141 ms (67.85%), Space: 19.1 MB (78.12%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -5,14 +5,14 @@ class Solution:
         n=len(ratings)
         arr=[1]*n
         
-        for i in range(n):
+        for i in range(1,n):
             
-            if i>0 and ratings[i]>ratings[i-1]:
+            if  ratings[i]>ratings[i-1]:
                 arr[i]=arr[i-1]+1
                 
-        for i in range(n-1,-1,-1):
+        for i in range(n-2,-1,-1):
             
-            if i<n-1 and ratings[i]>ratings[i+1]:
+            if  ratings[i]>ratings[i+1]:
                 arr[i]=max(arr[i],arr[i+1]+1)
         return sum(arr)        
                 

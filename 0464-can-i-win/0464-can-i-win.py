fc9214419ea4d3e21e94a2d1f063af14b0e4bf3e
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-11-17 01:01:57 +0530

    Time: 2320 ms (51.74%), Space: 193.8 MB (26.79%) - LeetHub

---
+++ 0464-can-i-win/0464-can-i-win.py
@@ -0,0 +1,74 @@
+class Solution:
+    def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
+        
+        
+        
+        dp={}
+        
+        def dfs(choices,remainder):
+            
+            if choices[-1]>=remainder:
+                return True
+            
+            state=tuple(choices)
+            if state in dp:
+                return dp[state]
+            
+            
+            for i in range(len(choices)):
+                
+                if not dfs(choices[:i]+choices[i+1:],remainder-choices[i]):
+                    dp[state]=True
+                    
+                    return True
+            dp[state]=False    
+            return False
+        
+        
+        
+            
+            
+        
+        
+        n=maxChoosableInteger
+        
+        if (n*(n+1))/2<desiredTotal:
+            return False
+        
+        choices=list(range(1,maxChoosableInteger+1))
+        return dfs(choices,desiredTotal)
+        
+
+#         m=desiredTotal              ### check the approach
+        
+#         l,r=1,maxChoosableInteger
+        
+#         cnt=1
+        
+#         while l<=r:
+            
+#             if cnt%2:
+                
+#                 if r>=m:
+#                     return True
+#                 else:
+#                     m-=l
+#                     l+=1
+#             else:
+                
+#                 if r>=m:
+#                     return False
+                
+#                 else:
+#                     m-=l
+#                     l+=1
+#             cnt+=1        
+                    
+            
+            
+        
+        
+       
+                
+                
+        

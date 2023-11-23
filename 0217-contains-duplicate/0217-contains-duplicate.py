d782b67023632f347a262ffa7f7599d98187b917
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-11-23 18:04:19 +0530

    Time: 460 ms (94.47%), Space: 31.9 MB (25.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,17 +4,31 @@ class Solution:
         
         
         
-        nums.sort()
+#         nums.sort()
         
-        l,r=0,1
+#         l,r=0,1
         
         
-        while r<len(nums):    
+#         while r<len(nums):           ### tc o(n log n )  sc o(1)  
             
-            if nums[l]==nums[r]:
-                return True
+#             if nums[l]==nums[r]:
+#                 return True
             
-            l+=1
-            r+=1
-        return False   
+#             l+=1
+#             r+=1
+#         return False   
+
+            x=set()
+          
+            for num in nums:
+                
+                if num in x:
+                    return True
+                x.add(num)
+            return False      
+                
+                
+                
+                
+                
         

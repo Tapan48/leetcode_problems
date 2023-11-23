ecfd98d22ca2ab9eaa3b3647751f291b35c54f12
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-11-23 18:02:11 +0530

    Time: 512 ms (10.64%), Space: 28.4 MB (91.80%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,20 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        
+        
+        
+        nums.sort()
+        
+        l,r=0,1
+        
+        
+        while r<len(nums):    
+            
+            if nums[l]==nums[r]:
+                return True
+            
+            l+=1
+            r+=1
+        return False   
+        

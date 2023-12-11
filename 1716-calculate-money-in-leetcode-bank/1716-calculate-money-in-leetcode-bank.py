ad0dadc6b2241137e84eabe84ed98cba0810949e
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-12-11 14:44:54 +0530

    Time: 49 ms (7.35%), Space: 16.4 MB (22.66%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,10 +1,34 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        a = n // 7
-        nt = n % 7
+#         a = n // 7
+#         nt = n % 7
 
-        ans1 = (nt * ((2 * (a + 1)) + (nt - 1))) // 2
-        ans2 = a * (2 * 28 + 7 * (a - 1)) // 2  
+#         ans1 = (nt * ((2 * (a + 1)) + (nt - 1))) // 2
+#         ans2 = a * (2 * 28 + 7 * (a - 1)) // 2  
 
-        return ans1 + ans2       
+#         return ans1 + ans2  
+          
+          res=0
+          day=1
+          deposit=1
+        
+          while day<=n:
+                
+                
+                res+=deposit
+                deposit+=1
+                
+                
+                if day%7==0:
+                    
+                    deposit=day//7+1
+                day+=1    
+          return res          
+                
+                
+                
+                
+                
+                
+                
        

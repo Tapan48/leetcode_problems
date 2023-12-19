943ce24e10ba4acad20e7683a6c5e13db78eadc0
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2023-12-19 12:17:28 +0530

    Time: 162 ms (27.83%), Space: 17.8 MB (35.41%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
@@ -2,7 +2,25 @@ class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
         
         
-        nums.sort()
-        n=len(nums)-1
-        ans=(nums[n]*nums[n-1])-(nums[1]*nums[0])
-        return ans
+#         nums.sort()
+#         n=len(nums)-1
+#         ans=(nums[n]*nums[n-1])-(nums[1]*nums[0])
+#         return ans
+
+          
+            heapq.heapify(nums)
+
+            s1=heapq.heappop(nums)
+            s2=heapq.heappop(nums)
+            
+            
+            negated_nums = [-num for num in nums]
+
+
+            heapq.heapify(negated_nums)
+            
+            l2=abs(heapq.heappop(negated_nums))
+            l1=abs(heapq.heappop(negated_nums))    
+            
+            ans=(l2*l1)-(s1*s2)
+            return ans

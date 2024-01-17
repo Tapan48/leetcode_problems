bbfc74cf4a879d37515f8e1e76b3ec37e5096e33
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2024-01-17 14:11:47 +0530

    Time: 338 ms (96.06%), Space: 32.8 MB (97.15%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -6,38 +6,54 @@
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
         
+        slow, fast = head, head
+        prev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        res = 0
+        while slow:
+            res = max(res, prev.val + slow.val)
+            prev = prev.next
+            slow = slow.next
+        return res
         
-            ptr = head
-            n = 0
-            while ptr:  # found length
-                n += 1
-                ptr = ptr.next
+        
+#             ptr = head
+#             n = 0
+#             while ptr:  # found length
+#                 n += 1
+#                 ptr = ptr.next
 
-            ptr = head
-            x = n // 2
-            while x:  # found middle half
-                ptr = ptr.next
-                x -= 1
+#             ptr = head
+#             x = n // 2
+#             while x:  # found middle half
+#                 ptr = ptr.next
+#                 x -= 1
 
-            prev = None  # Reverse the second half
-            while ptr:
-                next_node = ptr.next
-                ptr.next = prev
-                prev = ptr
-                ptr = next_node
+#             prev = None  # Reverse the second half
+#             while ptr:
+#                 next_node = ptr.next
+#                 ptr.next = prev
+#                 prev = ptr
+#                 ptr = next_node
 
-            y = n // 2
+#             y = n // 2
 
-            maxsum = 0
-            while y:
-                cursum = head.val + prev.val
-                head = head.next
-                prev = prev.next
+#             maxsum = 0   #### find maximum twin sum
+#             while y:
+#                 cursum = head.val + prev.val
+#                 head = head.next
+#                 prev = prev.next
 
-                maxsum = max(cursum, maxsum)
-                y -= 1
+#                 maxsum = max(cursum, maxsum)
+#                 y -= 1
 
-            return maxsum
+#             return maxsum
 
         
         

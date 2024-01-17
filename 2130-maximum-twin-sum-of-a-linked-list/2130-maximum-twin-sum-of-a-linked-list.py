3a26e73e1eec395618823d0c6599d3805c2a9511
Tapan Garasangi#48 <82650080+Tapan48@users.noreply.github.com>
2024-01-17 13:02:36 +0530

    Time: 370 ms (80.21%), Space: 39.2 MB (72.21%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -7,28 +7,80 @@ class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
         
         
+            ptr = head
+            n = 0
+            while ptr:  # found length
+                n += 1
+                ptr = ptr.next
+
+            ptr = head
+            x = n // 2
+            while x:  # found middle half
+                ptr = ptr.next
+                x -= 1
+
+            prev = None  # Reverse the second half
+            while ptr:
+                next_node = ptr.next
+                ptr.next = prev
+                prev = ptr
+                ptr = next_node
+
+            y = n // 2
+
+            maxsum = 0
+            while y:
+                cursum = head.val + prev.val
+                head = head.next
+                prev = prev.next
+
+                maxsum = max(cursum, maxsum)
+                y -= 1
+
+            return maxsum
+
         
-        ptr=head
-        arr=[]
-        while ptr:
-            
-            arr.append(ptr.val)
-            
-            ptr=ptr.next
-            
-        maxsum=0
-        n=len(arr)
         
-        l,r=0,n-1
         
-        while l<r:
+        
+        
+        
+#         ptr=head
+#         arr=[]
+#         while ptr:         ### tc o(n)  sc o(n)
             
-            cursum=arr[l]+arr[r]
-            maxsum=max(cursum,maxsum)
+#             arr.append(ptr.val)
             
-            l+=1
-            r-=1
-        return maxsum     
+#             ptr=ptr.next
+            
+#         maxsum=0
+#         n=len(arr)
+        
+#         l,r=0,n-1
+        
+#         while l<r:
+            
+#             cursum=arr[l]+arr[r]
+#             maxsum=max(cursum,maxsum)
+            
+#             l+=1
+#             r-=1
+#         return maxsum  
+
+
+
+
+
+            
+            
+            
+            
+            
+          
+          
+          
+         
+          
             
             
        
